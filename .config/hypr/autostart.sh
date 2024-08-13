55285652cc97727297612eeb600e98f25e6368ee
Nutty-Nuts <vegagerard1130@gmail.com>
2024-08-13 20:44:49 +0800

    added hyprland

---
+++ .config/hypr/autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sleep 2
+hyprctl dispatch exec [workspace special:todoist silent] flatpak run com.todoist.Todoist
+sleep 2
+hyprctl dispatch exec [workspace special:discord silent] flatpak run io.github.spacingbat3.webcord
