6ee719c57e5468266b56cf49c2247d17326c3b11
Nutty-Nuts <vegagerard1130@gmail.com>
2024-08-15 21:45:55 +0800

    implemented smooth slider animation for speaker osd

--- .config/ags/osd/speaker_osd.js
+++ .config/ags/osd/speaker_osd.js
@@ -1,12 +1,43 @@
 import GLib from 'gi://GLib'
 const audio = await Service.import('audio')
 
+let transition_interval;
+let slider_value = Variable(0.5);
+
 const SpeakerVolumeSlider = Widget.Slider({
     class_name: 'speaker-osd-slider unset',
     hexpand: true,
     draw_value: false,
-    value: audio['speaker'].bind('volume'),
+    // value: audio['speaker'].bind('volume'),
+    value: slider_value.bind(),
     on_change: ({ value }) => audio['speaker'].volume = value,
+}).hook(audio, (self) => {
+    let volume = audio['speaker'].volume 
+    let gap = Math.abs(slider_value.value - volume)
+    console.log('gap', gap)
+
+    if (transition_interval != null) {
+        GLib.source_remove(transition_interval)
+    }
+
+    if (self.value < volume) {
+        let transition_interval = Utils.interval(50, () => {
+            self.value = Math.min(self.value + 0.01, volume) 
+
+            if (self.value == volume) {
+                GLib.source_remove(transition_interval)
+            }
+        })
+    }
+    if (self.value > volume) {
+        let transition_interval = Utils.interval(50, () => {
+            self.value = Math.max(self.value - 0.01, volume) 
+
+            if (self.value == volume) {
+                GLib.source_remove(transition_interval)
+            }
+        })
+    }
 })
 
 const SpeakerIcon = Widget.Label({
@@ -28,7 +59,8 @@ export const SpeakerOSDWindow = Widget.Window({
     visible: false,
     layer: 'overlay',
     child: SpeakerOSD,
-    anchor: ["bottom"]
+    anchor: ["bottom"],
+    margins: [0, 0, 64, 0]
 })
 
 let hideTimeoutId;
