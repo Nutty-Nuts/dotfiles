6d9fb255fe0b600d1246c3b5b3d9e50e8c2a04d8
Nutty-Nuts <vegagerard1130@gmail.com>
2024-08-18 12:43:36 +0800

    format: applied formatting

--- .config/ags/bar/modules/battery.js
+++ .config/ags/bar/modules/battery.js
@@ -1,114 +1,113 @@
-const battery = await Service.import("battery")
+const battery = await Service.import("battery");
 
 // initial lines: 58
 // updated lines: 55
 
 function set_battery_state(widget, battery_state) {
     let battery_states = {
-        "charging": false,
-        "good": false,
-        "warning": false,
-        "critical": false,
-    }
-    battery_states[battery_state] = true
+        charging: false,
+        good: false,
+        warning: false,
+        critical: false,
+    };
+    battery_states[battery_state] = true;
 
-    widget.toggleClassName('charging', battery_states['charging'])
-    widget.toggleClassName('good', battery_states['good'])
-    widget.toggleClassName('warning', battery_states['warning'])
-    widget.toggleClassName('critical', battery_states['critical'])
+    widget.toggleClassName("charging", battery_states["charging"]);
+    widget.toggleClassName("good", battery_states["good"]);
+    widget.toggleClassName("warning", battery_states["warning"]);
+    widget.toggleClassName("critical", battery_states["critical"]);
 }
 
 export function Battery() {
     const battery_icon = Widget.Label({
-        class_name: 'battery-font circular-progress-status font',
-        label: '󰁹'
+        class_name: "battery-font circular-progress-status font",
+        label: "󰁹",
     }).hook(battery, (self) => {
-        let percent = battery.percent
-        let charging = battery.charging
+        let percent = battery.percent;
+        let charging = battery.charging;
 
         if (charging) {
-            self.label = '󱐋'
-            return
+            self.label = "󱐋";
+            return;
         }
         if (percent <= 25) {
-            self.label = '󰁼'
-            return } 
-        if (percent <= 50) {
-            self.label = '󰁾'
-            return
+            self.label = "󰁼";
+            return;
         }
-        self.label = '󰁹'
-    })
+        if (percent <= 50) {
+            self.label = "󰁾";
+            return;
+        }
+        self.label = "󰁹";
+    });
 
     const battery_label = Widget.Label({
-        class_name: 'battery-font circular-progress-status font',
-    }).on('realize', (self) => {
-        self.visible = false
-    }).hook(battery, (self) => {
-        self.label = `${Math.round(battery.percent)}`
+        class_name: "battery-font circular-progress-status font",
     })
+        .on("realize", (self) => {
+            self.visible = false;
+        })
+        .hook(battery, (self) => {
+            self.label = `${Math.round(battery.percent)}`;
+        });
 
     const battery_info_box = Widget.Box({
-        children: [
-            battery_icon,
-            battery_label
-        ],
+        children: [battery_icon, battery_label],
         homogeneous: true,
-    })
+    });
 
     const battery_circular = Widget.CircularProgress({
-        class_name: 'circular-progress-status',
-        value: battery.bind("percent").as(p => p > 0 ? p / 100 : 0),
+        class_name: "circular-progress-status",
+        value: battery.bind("percent").as((p) => (p > 0 ? p / 100 : 0)),
         rounded: true,
         startAt: 0.25,
-        child: battery_info_box
+        child: battery_info_box,
     }).hook(battery, (self) => {
-        let charging = battery.charging
-        let percent = battery.percent
+        let charging = battery.charging;
+        let percent = battery.percent;
 
         if (charging) {
-            set_battery_state(self, 'charging')
-            return
+            set_battery_state(self, "charging");
+            return;
         }
         if (percent <= 25) {
-            set_battery_state(self, 'critical')
-            return
-        } 
-        if (percent <= 50) {
-            set_battery_state(self, 'warning')
-            return
+            set_battery_state(self, "critical");
+            return;
         }
-        set_battery_state(self, 'good')
-    })
+        if (percent <= 50) {
+            set_battery_state(self, "warning");
+            return;
+        }
+        set_battery_state(self, "good");
+    });
 
     const battery_event_box = Widget.EventBox({
         child: battery_circular,
 
         on_hover: () => {
-            battery_icon.visible = false
-            battery_label.visible = true
+            battery_icon.visible = false;
+            battery_label.visible = true;
         },
         on_hover_lost: () => {
-            battery_icon.visible = true
-            battery_label.visible = false
-        }
+            battery_icon.visible = true;
+            battery_label.visible = false;
+        },
     }).hook(battery, (self) => {
-        let time_remaining = battery['time_remaining']
-        let seconds = time_remaining % 60
-        let minutes = (time_remaining - seconds) / 60 % 60
-        let hours = Math.floor((time_remaining - (minutes * 60)) / 60 / 60)
-        let message = ''
+        let time_remaining = battery["time_remaining"];
+        let seconds = time_remaining % 60;
+        let minutes = ((time_remaining - seconds) / 60) % 60;
+        let hours = Math.floor((time_remaining - minutes * 60) / 60 / 60);
+        let message = "";
 
         // console.log(time_remaining, 'time_remaining', seconds, minutes, hours)
         if (battery.charging) {
-            message = 'Until Fully Charged'
-        }
-        else {
-            message = 'Remaining'
+            message = "Until Fully Charged";
+        } else {
+            message = "Remaining";
         }
 
-        self.tooltip_text = `${String(hours).padStart(2, '0')}h ${String(minutes).padStart(2, '0')}m ${message}`
-    })
+        self.tooltip_text = `${String(hours).padStart(2, "0")}h ${String(minutes).padStart(2, "0")}m ${message}`;
+    });
 
-    return battery_event_box
+    return battery_event_box;
 }
--- .config/ags/events/battery_events.js
+++ .config/ags/events/battery_events.js
@@ -1,50 +1,48 @@
-const battery = await Service.import("battery")
-const sfx_theme = 'ocean'
-const default_sfx_format = 'oga'
-const sfx = `${App.configDir}/assets/sfx/${sfx_theme}`
+const battery = await Service.import("battery");
+const sfx_theme = "ocean";
+const default_sfx_format = "oga";
+const sfx = `${App.configDir}/assets/sfx/${sfx_theme}`;
 
 let previous_charging_state;
 let critical = false;
 
 function notify(icon, title, description) {
-    Utils.execAsync(`notify-send -i '${icon}' '${title}' '${description}'`)
+    Utils.execAsync(`notify-send -i '${icon}' '${title}' '${description}'`);
 }
 
 function play_sfx(sfx_name, audio_format = default_sfx_format) {
-    console.log(`${sfx_name}.${audio_format}`)
-    Utils.execAsync(`play ${sfx}/${sfx_name}.${audio_format}`)
+    console.log(`${sfx_name}.${audio_format}`);
+    Utils.execAsync(`play ${sfx}/${sfx_name}.${audio_format}`);
 }
 
 export function battery_events() {
-    Utils.merge([battery.bind('charging'), battery.bind('percent')], (charging, percent) => {
-        console.log('charging state changed')
+    Utils.merge([battery.bind("charging"), battery.bind("percent")], (charging, percent) => {
+        console.log("charging state changed");
         if (percent >= 0 && percent <= 25 && critical == false) {
-            play_sfx('battery-low')
-            notify('battery-low', 'Battery Critical', 'Battery is under 20%.')
+            play_sfx("battery-low");
+            notify("battery-low", "Battery Critical", "Battery is under 20%.");
 
-            critical = true
+            critical = true;
         }
         if (percent > 25 && critical == true) {
-            critical = false
+            critical = false;
         }
 
         if (previous_charging_state == charging) {
-            return
+            return;
         }
         if (previous_charging_state == false) {
-            play_sfx('power-plug')
-            notify('battery', 'Charger Connected', `Battery charging from ${percent}%`)
+            play_sfx("power-plug");
+            notify("battery", "Charger Connected", `Battery charging from ${percent}%`);
         }
         if (previous_charging_state == true) {
-            play_sfx('power-unplug')
-            notify('battery', 'Charger Disconnected', `Battery draining from ${percent}%`)
+            play_sfx("power-unplug");
+            notify("battery", "Charger Disconnected", `Battery draining from ${percent}%`);
         }
-        if (previous_charging_state == true && percent >= 80) { 
-            play_sfx('battery-full')
-            notify('battery', 'Charging Suspended', `Charging suspended at ${percent}%`)
+        if (previous_charging_state == true && percent >= 80) {
+            play_sfx("battery-full");
+            notify("battery", "Charging Suspended", `Charging suspended at ${percent}%`);
         }
-        previous_charging_state = charging
-    })
+        previous_charging_state = charging;
+    });
 }
-
-
