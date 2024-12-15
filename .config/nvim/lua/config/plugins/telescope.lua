ed74a713ac9434fd49fe00032aeed058889f7a62
Nutty-Nuts <vegagerard1130@gmail.com>
2024-12-15 14:37:34 +0800

    basic installation of telescope.nvim

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -5,6 +5,43 @@ return {
 		dependencies = {
 			'nvim-lua/plenary.nvim',
 			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-		}
+		},
+		config = function()
+			require('telescope').setup {
+				defaults = {
+					path_display = { "smart" },
+					theme = "dropdown",
+				},
+				pickers = {
+					find_files = {
+						theme = "dropdown",
+						selection_caret = "󰐊 ",
+						prompt_prefix = "   ",
+						follow = true,
+					},
+					buffers = {
+						theme = "dropdown",
+						selection_caret = "󰐊 ",
+						prompt_prefix = "   ",
+					},
+					live_grep = {
+						theme = "dropdown",
+						selection_caret = "󰐊 ",
+						prompt_prefix = "   ",
+						follow = true,
+					},
+				},
+			}
+
+			vim.keymap.set("n", "<leader>ff", require('telescope.builtin').find_files)
+			vim.keymap.set("n", "<leader>fg", require('telescope.builtin').live_grep)
+			vim.keymap.set("n", "<leader>fb", require('telescope.builtin').buffers)
+
+			vim.keymap.set("n", "<leader>fcg", function()
+				require('telescope.builtin').find_files {
+					cwd = vim.fn.stdpath("config")
+				}
+			end)
+		end
 	}
 }
